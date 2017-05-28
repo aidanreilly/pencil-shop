57f7def03010d8e42a0e02bcc8c860e20343aa84
aidanreilly <aidreilly@gmail.com>
2017-05-28 21:19:13 +0100

    submitting

--- app.js
+++ app.js
@@ -1,19 +1,61 @@
+/**
+ * @file main application file for the pencil shop node.js application
+ * @author Aidan Reilly 
+ */
+
+/**
+ * http module is for sending and receiving http traffic in the node application. The http server processes requests and responses. The createServer method creates the server and sets it up to process requests and responses. The url var is parsed from the http req input. The request and URL is pushed to the console. The main body of the application is set up in the server object here. If the method is a POST method and has a "/" case encoded in the URL, print to the console, process the req request. The request on method processes the request body, binds it to the POST event. The various methods GET methods grab the content from the DB and the push to the the browser.
+ * @module  http
+ */
 var http = require('http');
+
+/**
+ * mysql module connects the node application to the running DB, allowing data to nbe sent over and back. The connection uses the mysql connection to the db, with user root, default password, products DB. In production, this open pasword would be very dangerous.
+ * @module  mysql
+ */
 var mysql = require('mysql');
+
+/**
+ * fs is for file i/o operations
+ * @module fs
+ */
 var fs = require('fs');
+
+/**
+ * Path is for manipulating file paths, finding the basename, file type, etc.
+ * @module path
+ */
 var path = require('path');
-var static = require('node-static');
+
+/**
+ * imgserver is for serving static resources, html, links, images. Open on port 5000. Uses the node-static module. Images are served from inside the /public folder. only resources in this folder are served. 
+ *  @module  imgserver
+ */
+var imgserver = require('node-static');
+
+/*
+ * The url module provides utilities for URL resolution and parsing. 
+ * @module node-static
+ */
 var url = require('url');
+
+/*
+ * Resource is for the export modules that are used in the application they are called from resource.js in the main folder.
+ * @module resource
+ */
 var resource = require('./resource');
 
-
-//set up the db connection
+/*
+ * Connection for the MySQL DB.
+ * @module connection
+ */
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'products'
 });
+
 
 connection.connect(function(err) {
     if (!err) {
@@ -24,15 +66,19 @@ connection.connect(function(err) {
 });
 
 //serve any images in the public folder on port
-var fileServer = new static.Server('./public');
-/*
-The fileServer function
- */
+
+var fileServer = new imgserver.Server('./public');
+
 http.createServer(function(req, res) {
     fileServer.serve(req, res);
 
 }).listen(5000);
 
+
+//main body of application routing
+/*
+ *Server application runs the show and route the applcation res and req methods, access to the DB, etc.
+ */
 var server = http.createServer(function(req, res) {
     switch (req.method) {
         case 'POST':
@@ -48,7 +94,7 @@ var server = http.createServer(function(req, res) {
                     break;
                 case '/remove':
                     resource.remove(connection, req, res);
-                    break;                    
+                    break;
                 case '/update':
                     resource.update(connection, req, res);
                     break;
--- readme.md
+++ readme.md
@@ -1,38 +1,39 @@
-#Node.JS Assignment 2 - Pencil me in onlien shop
-Simple online shop with Node.js and SQL.
+#Overview of the online shop node.js application
 
-Node modules are included in /modules folder. 
+The application is extensively commented, please see the jsdoc doc below for each of app.js and resource.js for further commented detail. 
 
-##Shop requirements
-* Customer user can view catalog, buy items
-* Owner can view catalog, and view orders (time stamp, prod ID, etc)
-* Nav bar
-* Products DB table
-* Orders table
-* products  - unique id
-* page footer
-* product objects - unique ids
-* browse page
+* out/app.js.html - main application file.
+* out/resource.js.html - contains the bulk of the code which was designed around a number of key module exports which are called in the main app.js file. 
+* stationery.sql  - MySQL database create script included in the main project folder. This was run against WAMP 3.0.6 with MySQL version 5.7.14  
+
+The shop application works as follows:
+
+The node server is created and launches the home page. The homepage is generated from a header and footer html file and a read on a MySQL DB.  POST actions are used to push new content to the DB. jQuery reads values from the application pages, parses the values, and pushes them to the DB. GET actions are used to push data from the DB to the browser. A read is done of the various tables in the Products database every time the page is refreshed. 
+
+When an entry is added to the orders or pencils tables, a unique ID is generated. Users can add, update and delete entries in the SQL tables.  
+
+Node modules are included in /modules folder. a package.json is available in the main project folder. The node modules used are:
+* node-static - this is used to serve resources. 
+* mysql - used to talk to the MySQL DB.
+
+#Features of the site:
+Some of these features are not fully implemented.
+
+* Users can view the product catalog and buy items, adding multiple items to their cart.
+* The site has a simple nav bar
+* The Products database has the following tables:
+    -   Orders table
+    -   Pencils table
+* Browse page
     - add to cart button
-    - cart page - allow updates with quantities
+* Cart page 
+    - allow updates with quantities
     - buy now link
-* Back office for owner to manage stock levels
+* Back office page 
+    - for owner to manage stock levels
 
-##Connection to WAMP MYSQL DB
-open up wamp or cmd-line:
+This doc was built with jsdoc.
 
-    mysql -u root -p
-    root: <empty>
+    jsdoc studentrecs.js -d out  -r --readme README.md
 
-Import the db at cmd-line:
-
-    source products.sql
-
-pencil pics cogged from:
-https://cwpencils.com/
-
-Overview doc built with jsdoc.
-
-    jsdoc app.js -d out  -r --readme README.md
-
-output is at out/index.html
+Output is at out/index.html
